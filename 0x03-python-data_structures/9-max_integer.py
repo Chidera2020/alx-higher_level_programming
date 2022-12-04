dc777a30fab6f493ea64276064887479d7ce4211
Chidera2020 <c.chideranaomi@gmail.com>
2022-12-04 03:35:23 -0800

    done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if (len(my_list) == 0):
+        if (len(my_list) == 0):
+            return 0
+        else:
+            my_list.sort()
+            return (my_list[len(my_list) - 1])
